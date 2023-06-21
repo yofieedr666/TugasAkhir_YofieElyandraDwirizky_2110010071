fdf1646c75002e7f49d80ac638efdfa11cc08300
yofieedr666 <yofiemadridista@gmail.com>
2023-06-21 12:59:41 +0800

    Membuat Form Data Siswa

--- datasiswa.pas
+++ datasiswa.pas
@@ -1,4 +1,4 @@
-unit datawalikelas;
+unit datasiswa;
 
 interface
 
@@ -7,29 +7,41 @@ uses
   Dialogs, Grids, DBGrids, StdCtrls, ExtCtrls;
 
 type
-  TForm1 = class(TForm)
-    Panel1: TPanel;
+  TForm2 = class(TForm)
     Label1: TLabel;
+    Panel1: TPanel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
+    Label13: TLabel;
+    Label14: TLabel;
     Edit1: TEdit;
     Edit2: TEdit;
     Edit3: TEdit;
     Edit4: TEdit;
-    Label5: TLabel;
-    Label6: TLabel;
-    Label7: TLabel;
     Edit5: TEdit;
     Edit6: TEdit;
     Edit7: TEdit;
+    Edit8: TEdit;
+    Edit9: TEdit;
+    Edit10: TEdit;
+    Edit11: TEdit;
+    Edit12: TEdit;
+    Edit13: TEdit;
     Button1: TButton;
     Button2: TButton;
     Button3: TButton;
     Button4: TButton;
     Button5: TButton;
     DBGrid1: TDBGrid;
-    Label8: TLabel;
   private
     { Private declarations }
   public
@@ -37,7 +49,7 @@ type
   end;
 
 var
-  Form1: TForm1;
+  Form2: TForm2;
 
 implementation
 
